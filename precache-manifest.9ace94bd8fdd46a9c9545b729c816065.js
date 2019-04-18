a4fe1a0433a07c2543354a3460d1bea0cdc91605
Daniel <danielmomusic@gmail.com>
2019-04-18 08:29:15 -0500

    Updates

---
+++ precache-manifest.9ace94bd8fdd46a9c9545b729c816065.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "571b2285a49433bf1f4b",
+    "url": "/github-upload/static/css/main.e69555d0.chunk.css"
+  },
+  {
+    "revision": "571b2285a49433bf1f4b",
+    "url": "/github-upload/static/js/main.15221d97.chunk.js"
+  },
+  {
+    "revision": "707f6502d882d4f5dd79",
+    "url": "/github-upload/static/js/runtime~main.912da764.js"
+  },
+  {
+    "revision": "04f8551ddd4f9e273eaa",
+    "url": "/github-upload/static/js/2.f7f5fe26.chunk.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/github-upload/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/github-upload/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/github-upload/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "bb967f6fd9e25b5d60b8392ad8e236f2",
+    "url": "/github-upload/index.html"
+  }
+];
