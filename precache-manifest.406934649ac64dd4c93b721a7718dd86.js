0ebeb04c70a5899b74a04190d011eb4dfe65c06c
Daniel <danielmomusic@gmail.com>
2019-04-20 07:17:55 -0500

    Updates

---
+++ precache-manifest.406934649ac64dd4c93b721a7718dd86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be06d4a17863db9904a",
+    "url": "/react-site-1/static/css/main.eff3c763.chunk.css"
+  },
+  {
+    "revision": "6be06d4a17863db9904a",
+    "url": "/react-site-1/static/js/main.73f0604f.chunk.js"
+  },
+  {
+    "revision": "f658d137a21394c75d6c",
+    "url": "/react-site-1/static/js/runtime~main.08ba4e01.js"
+  },
+  {
+    "revision": "0269475c551057d58ac8",
+    "url": "/react-site-1/static/js/2.a3e3cf22.chunk.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/react-site-1/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/react-site-1/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/react-site-1/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "81a4204b6c0d108d4be3ebf165333d58",
+    "url": "/react-site-1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github-upload/precache-manifest.4b8c4d98ac140f5398ef182779f4ea9d.js"
+  "/react-site-1/precache-manifest.406934649ac64dd4c93b721a7718dd86.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github-upload/index.html", {
+workbox.routing.registerNavigationRoute("/react-site-1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
